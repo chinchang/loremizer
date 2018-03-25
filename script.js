f4ad0446e84cf241af290082e788a304780f2f2d
Kushagra Gour <chinchang457@gmail.com>
2018-03-26 03:19:48 +0530

    more handling for input and iframe

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc non est positum in nostra actione. Dat enim intervalla et relaxat. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quasi vero, inquit, perpetua oratio rhetorum solum, non etiam philosophorum sit. Pauca mutat vel plura sane; Ne tum quidem te respicies et cogitabis sibi quemque natum esse et suis voluptatibus? Duo Reges: constructio interrete. Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium.'
+// https://loripsum.net/
 
 var imgs = [...document.querySelectorAll('img')];
 
@@ -15,30 +16,45 @@ function replaceImgs() {
   })
 }
 
+function handleIframes() {
+  var iframes = [...document.querySelectorAll('iframe')];
+  iframes.forEach(iframe => {
+    iframe.style.filter = 'blur(4px)'
+  })
+}
+
+function handleFormInputs() {
+  var inputs = [...document.querySelectorAll('input, textarea')];
+  inputs.forEach(input => {
+    input.value = lorem.substr(0, input.value.length)
+  })
+}
+
 function replaceBgImgs() {
   var allBackgroundURLs = new Array();
-    var tags = [...document.querySelectorAll('*')];
-    var numTags = tags.length;
-    for (var i = 0; i < numTags; i++) {
-        tag = tags[i];
-        if (tag.style.background.match("url") || tag.style.backgroundImage) {
-            var bg = tag.style.background;
-            // allBackgroundURLs.push({tag, bg.substr(bg.indexOf("url") + 4, bg.lastIndexOf(")") - (bg.indexOf("url") + 4) ) });
-            tag.style.backgroundImage = 'url(https://picsum.photos/200?random)';
-        }
+  var tags = [...document.querySelectorAll('*')];
+  var numTags = tags.length;
+  for (var i = 0; i < numTags; i++) {
+    tag = tags[i];
+    if (tag.style.background.match("url") || tag.style.backgroundImage) {
+      var bg = tag.style.background;
+      // allBackgroundURLs.push({tag, bg.substr(bg.indexOf("url") + 4, bg.lastIndexOf(")") - (bg.indexOf("url") + 4) ) });
+      tag.style.backgroundImage = 'url(https://picsum.photos/200?random)';
     }
+  }
 }
+
 function getTextNodesIn(elem, opt_fnFilter) {
   var textNodes = [];
   if (elem) {
     for (var nodes = elem.childNodes, i = nodes.length; i--;) {
-      var node = nodes[i], nodeType = node.nodeType;
+      var node = nodes[i],
+        nodeType = node.nodeType;
       if (nodeType == 3) {
         if (!opt_fnFilter || opt_fnFilter(node, elem)) {
           textNodes.push(node);
         }
-      }
-      else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+      } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
         textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
       }
     }
@@ -46,12 +62,13 @@ function getTextNodesIn(elem, opt_fnFilter) {
   return textNodes;
 }
 
-var nodes = getTextNodesIn(document.body).filter(n => n.nodeValue.match(/\S/) );
+var nodes = getTextNodesIn(document.body).filter(n => n.nodeValue.match(/\S/));
 // console.log(nodes.length, nodes[3].nodeValue)
 nodes.forEach(n => {
   n.nodeValue = lorem.substr(0, n.nodeValue.length)
 })
-replaceImgs()
-replaceBgImgs()
 
-
+requestIdleCallback(replaceImgs)
+requestIdleCallback(replaceBgImgs)
+requestIdleCallback(handleIframes)
+requestIdleCallback(handleFormInputs)
