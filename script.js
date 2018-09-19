5561d088df457e95d08274c8eedbf77868951c7e
Kushagra Gour <chinchang457@gmail.com>
2018-09-19 15:15:25 +0530

    add new icons and undo support

--- script.js
+++ script.js
@@ -1,74 +1,109 @@
-const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc non est positum in nostra actione. Dat enim intervalla et relaxat. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quasi vero, inquit, perpetua oratio rhetorum solum, non etiam philosophorum sit. Pauca mutat vel plura sane; Ne tum quidem te respicies et cogitabis sibi quemque natum esse et suis voluptatibus? Duo Reges: constructio interrete. Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium.'
-// https://loripsum.net/
+window.loremiscous =
+	window.loremiscous ||
+	(function() {
+		const lorem =
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc non est positum in nostra actione. Dat enim intervalla et relaxat. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quasi vero, inquit, perpetua oratio rhetorum solum, non etiam philosophorum sit. Pauca mutat vel plura sane; Ne tum quidem te respicies et cogitabis sibi quemque natum esse et suis voluptatibus? Duo Reges: constructio interrete. Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium.';
+		// https://loripsum.net/
 
-var imgs = [...document.querySelectorAll('img')];
+		var imgs = [...document.querySelectorAll('img')];
+		var operations = [];
 
-function replaceImgs() {
-  imgs.forEach(i => {
-    const width = +i.style.width;
-    const height = +i.style.height;
-    if (width) {
-      i.src = `https://picsum.photos/${width}/${height}?random`
-    } else {
-      i.src = `https://picsum.photos/200?random`
-    }
-    //    https://loremflickr.com/320/240
-  })
-}
+		function addOperation(fn) {
+			operations.push(fn);
+		}
 
-function handleIframes() {
-  var iframes = [...document.querySelectorAll('iframe')];
-  iframes.forEach(iframe => {
-    iframe.style.filter = 'blur(4px)'
-  })
-}
+		function replaceImgs() {
+			imgs.forEach(i => {
+				const width = +i.style.width;
+				const height = +i.style.height;
+				const lastValue = i.src;
+				addOperation(() => (i.src = lastValue));
+				if (width) {
+					i.src = `https://picsum.photos/${width}/${height}?random`;
+				} else {
+					i.src = `https://picsum.photos/200?random`;
+				}
+				//    https://loremflickr.com/320/240
+			});
+		}
 
-function handleFormInputs() {
-  var inputs = [...document.querySelectorAll('input, textarea')];
-  inputs.forEach(input => {
-    input.value = lorem.substr(0, input.value.length)
-  })
-}
+		function handleIframes() {
+			var iframes = [...document.querySelectorAll('iframe')];
+			iframes.forEach(iframe => {
+				iframe.style.filter = 'blur(4px)';
+			});
+		}
 
-function replaceBgImgs() {
-  var allBackgroundURLs = new Array();
-  var tags = [...document.querySelectorAll('*')];
-  var numTags = tags.length;
-  for (var i = 0; i < numTags; i++) {
-    tag = tags[i];
-    if (tag.style.background.match("url") || tag.style.backgroundImage) {
-      var bg = tag.style.background;
-      // allBackgroundURLs.push({tag, bg.substr(bg.indexOf("url") + 4, bg.lastIndexOf(")") - (bg.indexOf("url") + 4) ) });
-      tag.style.backgroundImage = 'url(https://picsum.photos/200?random)';
-    }
-  }
-}
+		function handleFormInputs() {
+			var inputs = [...document.querySelectorAll('input, textarea')];
+			inputs.forEach(input => {
+				input.value = lorem.substr(0, input.value.length);
+			});
+		}
 
-function getTextNodesIn(elem, opt_fnFilter) {
-  var textNodes = [];
-  if (elem) {
-    for (var nodes = elem.childNodes, i = nodes.length; i--;) {
-      var node = nodes[i],
-        nodeType = node.nodeType;
-      if (nodeType == 3) {
-        if (!opt_fnFilter || opt_fnFilter(node, elem)) {
-          textNodes.push(node);
-        }
-      } else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
-        textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
-      }
-    }
-  }
-  return textNodes;
-}
+		function replaceBgImgs() {
+			var allBackgroundURLs = new Array();
+			var tags = [...document.querySelectorAll('*')];
+			var numTags = tags.length;
+			for (var i = 0; i < numTags; i++) {
+				tag = tags[i];
+				if (tag.style.background.match('url') || tag.style.backgroundImage) {
+					var bg = tag.style.background;
+					// allBackgroundURLs.push({tag, bg.substr(bg.indexOf("url") + 4, bg.lastIndexOf(")") - (bg.indexOf("url") + 4) ) });
+					tag.style.backgroundImage = 'url(https://picsum.photos/200?random)';
+				}
+			}
+		}
 
-var nodes = getTextNodesIn(document.body).filter(n => n.nodeValue.match(/\S/));
-// console.log(nodes.length, nodes[3].nodeValue)
-nodes.forEach(n => {
-  n.nodeValue = lorem.substr(0, n.nodeValue.length)
-})
+		function getTextNodesIn(elem, opt_fnFilter) {
+			var textNodes = [];
+			if (elem) {
+				for (var nodes = elem.childNodes, i = nodes.length; i--; ) {
+					var node = nodes[i],
+						nodeType = node.nodeType;
+					if (nodeType == 3) {
+						if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+							textNodes.push(node);
+						}
+					} else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+						textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+					}
+				}
+			}
+			return textNodes;
+		}
 
-requestIdleCallback(replaceImgs)
-requestIdleCallback(replaceBgImgs)
-requestIdleCallback(handleIframes)
-requestIdleCallback(handleFormInputs)
+		function replaceText() {
+			var nodes = getTextNodesIn(document.body).filter(n =>
+				n.nodeValue.match(/\S/)
+			);
+			// console.log(nodes.length, nodes[3].nodeValue)
+			nodes.forEach(n => {
+				const lastValue = n.nodeValue;
+				addOperation(() => (n.nodeValue = lastValue));
+				n.nodeValue = lorem.substr(0, n.nodeValue.length);
+			});
+		}
+
+		function undo() {
+			operations.forEach(op => op());
+			operations = [];
+		}
+
+		function runLoremiscous() {
+			if (operations.length) {
+				undo();
+			} else {
+				requestIdleCallback(replaceText);
+				requestIdleCallback(replaceImgs);
+				requestIdleCallback(replaceBgImgs);
+				requestIdleCallback(handleIframes);
+
+				// TODO: Replacing form control values can trigger unexpected behavior.
+				// requestIdleCallback(handleFormInputs)
+			}
+		}
+		return runLoremiscous;
+	})();
+
+window.loremiscous();
