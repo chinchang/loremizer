b0990cdfcaea4647f20ce2a4dd14c3c1b67e7aee
Kushagra Gour <chinchang457@gmail.com>
2019-02-21 17:44:17 +0530

    feat: increase length of replaced text

--- popup.js
+++ popup.js
@@ -1,88 +1,97 @@
-const TITLE_APPLY = 'loremizer';
-const TITLE_REMOVE = 'Remove loremizer';
+const TITLE_APPLY = "loremizer";
+const TITLE_REMOVE = "Remove loremizer";
 
 const browser = window.chrome || browser;
 
 function getCurrentTab() {
-	return new Promise((resolve, reject) => {
-		if (window.chrome) {
-			browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-				resolve(tabs[0]);
-			});
-		} else {
-			return browser.tabs.query({ active: true, currentWindow: true });
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (window.chrome) {
+      browser.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        resolve(tabs[0]);
+      });
+    } else {
+      return browser.tabs.query({ active: true, currentWindow: true });
+    }
+  });
 }
 
 function getIconTitle(tabId) {
-	return new Promise(resolve => {
-		if (window.chrome) {
-			browser.browserAction.getTitle({ tabId: tabId }, resolve);
-		} else {
-			return browser.browserAction.getTitle({ tabId: tabId });
-		}
-	});
+  return new Promise(resolve => {
+    if (window.chrome) {
+      browser.browserAction.getTitle({ tabId: tabId }, resolve);
+    } else {
+      return browser.browserAction.getTitle({ tabId: tabId });
+    }
+  });
 }
 
 function executeScript(tabId, details) {
-	return new Promise(resolve => {
-		if (window.chrome) {
-			browser.tabs.executeScript(tabId, details, resolve);
-		} else {
-			return browser.tabs.executeScript(tabId, details);
-		}
-	});
+  return new Promise(resolve => {
+    if (window.chrome) {
+      browser.tabs.executeScript(tabId, details, resolve);
+    } else {
+      return browser.tabs.executeScript(tabId, details);
+    }
+  });
 }
 
 function toggle(tab) {
-	function gotTitle(title) {
-		if (title === TITLE_APPLY) {
-			browser.browserAction.setIcon({ tabId: tab.id, path: 'icons/on.png' });
-			browser.browserAction.setTitle({ tabId: tab.id, title: TITLE_REMOVE });
-			const selectedOptions = [
-				...document.querySelectorAll('input[type="checkbox"]:checked')
-			].map(input => input.name);
+  function gotTitle(title) {
+    if (title === TITLE_APPLY) {
+      browser.browserAction.setIcon({ tabId: tab.id, path: "icons/on.png" });
+      browser.browserAction.setTitle({ tabId: tab.id, title: TITLE_REMOVE });
+      const selectedOptions = [
+        ...document.querySelectorAll('input[type="checkbox"]:checked'),
+        ...document.querySelectorAll("[data-setting]")
+      ].reduce((obj, input) => {
+        obj[input.name] = input.value;
+        return obj;
+      }, {});
 
-			executeScript(tab.id, {
-				file: 'script.js'
-			}).then(() => {
-				executeScript(tab.id, {
-					code: `window.loremiscous(${JSON.stringify(selectedOptions)});`
-				});
-			});
-			onActivation();
-		} else {
-			browser.browserAction.setIcon({ tabId: tab.id, path: 'icons/off.png' });
-			browser.browserAction.setTitle({ tabId: tab.id, title: TITLE_APPLY });
-			executeScript(tab.id, {
-				code: `window.loremiscous();`
-			});
-			onDeactivation();
-		}
-	}
+      executeScript(tab.id, {
+        file: "script.js"
+      }).then(() => {
+        executeScript(tab.id, {
+          code: `window.loremiscous(${JSON.stringify(selectedOptions)});`
+        });
+      });
+      onActivation();
+    } else {
+      browser.browserAction.setIcon({ tabId: tab.id, path: "icons/off.png" });
+      browser.browserAction.setTitle({ tabId: tab.id, title: TITLE_APPLY });
+      executeScript(tab.id, {
+        code: `window.loremiscous();`
+      });
+      onDeactivation();
+    }
+  }
 
-	getIconTitle(tab.id).then(gotTitle);
+  getIconTitle(tab.id).then(gotTitle);
 }
 
 function btnClickHandler() {
-	getCurrentTab().then(tab => {
-		toggle(tab);
-	});
+  getCurrentTab().then(tab => {
+    toggle(tab);
+  });
 }
 function onActivation() {
-	document.body.classList.add('is-active');
+  document.body.classList.add("is-active");
 }
 function onDeactivation() {
-	document.body.classList.remove('is-active');
+  document.body.classList.remove("is-active");
 }
-activateBtn.addEventListener('click', btnClickHandler);
-deactivateBtn.addEventListener('click', btnClickHandler);
+
+activateBtn.addEventListener("click", btnClickHandler);
+deactivateBtn.addEventListener("click", btnClickHandler);
+textLengthMultiplierInput.addEventListener("change", e => {
+  textLengthMultiplierValue.textContent = `${e.target.value}x`;
+});
+textLengthMultiplierValue.textContent = `${textLengthMultiplierInput.value}x`;
 
 getCurrentTab().then(tab => {
-	getIconTitle(tab.id).then(title => {
-		if (title === TITLE_REMOVE) {
-			onActivation();
-		}
-	});
+  getIconTitle(tab.id).then(title => {
+    if (title === TITLE_REMOVE) {
+      onActivation();
+    }
+  });
 });
--- script.js
+++ script.js
@@ -1,186 +1,191 @@
 window.loremiscous =
-	window.loremiscous ||
-	(function() {
-		const lorem =
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc non est positum in nostra actione. Dat enim intervalla et relaxat. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quasi vero, inquit, perpetua oratio rhetorum solum, non etiam philosophorum sit. Pauca mutat vel plura sane Ne tum quidem te respicies et cogitabis sibi quemque natum esse et suis voluptatibus? Duo Reges constructio interrete. Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium.';
-		// https://loripsum.net/
-		const env = window.chrome || browser;
-		var imgs = [...document.querySelectorAll('img')];
-		var operations = [];
+  window.loremiscous ||
+  (function() {
+    const lorem =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hoc non est positum in nostra actione. Dat enim intervalla et relaxat. Cupit enim dícere nihil posse ad beatam vitam deesse sapienti. Quasi vero, inquit, perpetua oratio rhetorum solum, non etiam philosophorum sit. Pauca mutat vel plura sane Ne tum quidem te respicies et cogitabis sibi quemque natum esse et suis voluptatibus? Duo Reges constructio interrete. Ergo instituto veterum, quo etiam Stoici utuntur, hinc capiamus exordium.";
+    // https://loripsum.net/
+    const env = window.chrome || browser;
+    var imgs = [...document.querySelectorAll("img")];
+    var operations = [];
 
-		function addOperation(fn) {
-			operations.push(fn);
-		}
+    function addOperation(fn) {
+      operations.push(fn);
+    }
 
-		function replaceImgs() {
-			imgs.forEach(i => {
-				const width = +i.style.width;
-				const height = +i.style.height;
-				const lastValue = i.src;
-				addOperation(() => (i.src = lastValue));
-				if (width) {
-					i.src = `https://picsum.photos/${width}/${height}?random`;
-				} else {
-					i.src = `https://picsum.photos/200?random`;
-				}
-				//    https://loremflickr.com/320/240
-			});
-		}
+    function replaceImgs() {
+      imgs.forEach(i => {
+        const width = +i.style.width;
+        const height = +i.style.height;
+        const lastValue = i.src;
+        addOperation(() => (i.src = lastValue));
+        if (width) {
+          i.src = `https://picsum.photos/${width}/${height}?random`;
+        } else {
+          i.src = `https://picsum.photos/200?random`;
+        }
+        //    https://loremflickr.com/320/240
+      });
+    }
 
-		function handleIframes() {
-			var iframes = [...document.querySelectorAll('iframe')];
-			iframes.forEach(iframe => {
-				const lastValue = iframe.style.filter;
-				addOperation(() => (iframe.style.filter = lastValue));
-				iframe.style.filter = 'blur(4px)';
-			});
-		}
+    function handleIframes() {
+      var iframes = [...document.querySelectorAll("iframe")];
+      iframes.forEach(iframe => {
+        const lastValue = iframe.style.filter;
+        addOperation(() => (iframe.style.filter = lastValue));
+        iframe.style.filter = "blur(4px)";
+      });
+    }
 
-		function handleFormInputs() {
-			var inputs = [...document.querySelectorAll('input, textarea')];
-			inputs.forEach(input => {
-				const lastValue = input.value;
-				addOperation(() => (input.value = lastValue));
-				const inputName = input.name || '';
-				if (inputName.match(/(first-name|firstname|first_name)/)) {
-					input.value = 'John';
-				} else if (inputName.match(/(last-name|lastname|last_name)/)) {
-					input.value = 'Doe';
-				} else if (inputName.match(/name/)) {
-					input.value = 'John Doe';
-				} else if (input.type === 'email' || inputName.match(/mail/)) {
-					input.value = 'johndoe@gmail.com';
-				} else if (input.type === 'tel' || inputName.match(/(phone|mobile)/)) {
-					input.value = '0113246327846';
-				} else {
-					input.value = lorem.substr(0, input.value.length || 20);
-				}
-			});
-		}
+    function handleFormInputs() {
+      var inputs = [...document.querySelectorAll("input, textarea")];
+      inputs.forEach(input => {
+        const lastValue = input.value;
+        addOperation(() => (input.value = lastValue));
+        const inputName = input.name || "";
+        if (inputName.match(/(first-name|firstname|first_name)/)) {
+          input.value = "John";
+        } else if (inputName.match(/(last-name|lastname|last_name)/)) {
+          input.value = "Doe";
+        } else if (inputName.match(/name/)) {
+          input.value = "John Doe";
+        } else if (input.type === "email" || inputName.match(/mail/)) {
+          input.value = "johndoe@gmail.com";
+        } else if (input.type === "tel" || inputName.match(/(phone|mobile)/)) {
+          input.value = "0113246327846";
+        } else {
+          input.value = lorem.substr(0, input.value.length || 20);
+        }
+      });
+    }
 
-		function replaceBgImgs() {
-			var tags = [...document.querySelectorAll('*')];
-			var numTags = tags.length;
-			let computedStyles;
-			for (var i = 0; i < numTags; i++) {
-				const tag = tags[i];
-				computedStyles = window.getComputedStyle(tag);
-				if (computedStyles.backgroundImage !== 'none') {
-					const lastValue = computedStyles.backgroundImage;
-					addOperation(() => (tag.style.backgroundImage = lastValue));
-					tag.style.backgroundImage = 'url(https://picsum.photos/200?random)';
-				}
-			}
-		}
+    function replaceBgImgs() {
+      var tags = [...document.querySelectorAll("*")];
+      var numTags = tags.length;
+      let computedStyles;
+      for (var i = 0; i < numTags; i++) {
+        const tag = tags[i];
+        computedStyles = window.getComputedStyle(tag);
+        if (computedStyles.backgroundImage !== "none") {
+          const lastValue = computedStyles.backgroundImage;
+          addOperation(() => (tag.style.backgroundImage = lastValue));
+          tag.style.backgroundImage = "url(https://picsum.photos/200?random)";
+        }
+      }
+    }
 
-		function getTextNodesIn(elem, filterFn) {
-			var textNodes = [];
-			if (elem) {
-				for (var nodes = elem.childNodes, i = nodes.length; i--; ) {
-					var node = nodes[i],
-						nodeType = node.nodeType;
-					if (nodeType == 3) {
-						if (!filterFn || filterFn(node, elem)) {
-							textNodes.push(node);
-						}
-					} else if (
-						node.tagName !== 'SCRIPT' &&
-						node.tagName !== 'STYLE' &&
-						(nodeType == 1 || nodeType == 9 || nodeType == 11)
-					) {
-						textNodes = textNodes.concat(getTextNodesIn(node, filterFn));
-					}
-				}
-			}
-			return textNodes;
-		}
+    function getTextNodesIn(elem, filterFn) {
+      var textNodes = [];
+      if (elem) {
+        for (var nodes = elem.childNodes, i = nodes.length; i--; ) {
+          var node = nodes[i],
+            nodeType = node.nodeType;
+          if (nodeType == 3) {
+            if (!filterFn || filterFn(node, elem)) {
+              textNodes.push(node);
+            }
+          } else if (
+            node.tagName !== "SCRIPT" &&
+            node.tagName !== "STYLE" &&
+            (nodeType == 1 || nodeType == 9 || nodeType == 11)
+          ) {
+            textNodes = textNodes.concat(getTextNodesIn(node, filterFn));
+          }
+        }
+      }
+      return textNodes;
+    }
 
-		function replaceText() {
-			var nodes = getTextNodesIn(document.body).filter(n =>
-				n.nodeValue.match(/\S/)
-			);
-			// console.log(nodes.length, nodes[3].nodeValue)
-			nodes.forEach(n => {
-				const lastValue = n.nodeValue;
-				addOperation(() => (n.nodeValue = lastValue));
-				// replace numbers with numbers and strings with strings
-				n.nodeValue = n.nodeValue.replace(/\s+/g, ' ');
-				n.nodeValue = n.nodeValue
-					// Matches digits, non-whitespace special chacters, underscore or spacers
-					.split(/(\d+|[^\w\s]|_|\s)/)
-					.map(str => {
-						// If this is just numbers
-						if (parseInt(str, 10)) {
-							return parseInt(
-								Math.random()
-									.toString()
-									.slice(2, Math.min(str.length + 2, 18)),
-								10
-							);
-						} else if (
-							str.length === 1 &&
-							(str.match(/[^\w]/) || str === '_')
-						) {
-							// If single special character or _
-							return str;
-						} else {
-							return lorem
-								.split(' ')
-								.sort(() => (Math.random() > 0.5 ? 1 : -1))
-								.join(' ')
-								.substr(0, str.length);
-						}
-					})
-					.join('');
-			});
-		}
+    function replaceText({ textLengthMultiplier }) {
+      var nodes = getTextNodesIn(document.body).filter(n =>
+        n.nodeValue.match(/\S/)
+      );
+      // console.log(nodes.length, nodes[3].nodeValue)
+      nodes.forEach(n => {
+        const lastValue = n.nodeValue;
+        addOperation(() => (n.nodeValue = lastValue));
+        // replace numbers with numbers and strings with strings
+        n.nodeValue = n.nodeValue.replace(/\s+/g, " ");
+        n.nodeValue = n.nodeValue
+          // Matches digits, non-whitespace special chacters, underscore or spacers
+          .split(/(\d+|[^\w\s]|_|\s)/)
+          .map(str => {
+            const length = str.length * textLengthMultiplier;
+            // If this is just numbers
+            if (parseInt(str, 10)) {
+              return parseInt(
+                Math.random()
+                  .toString()
+                  .slice(2, Math.min(length + 2, 18)),
+                10
+              );
+            } else if (
+              str.length === 1 &&
+              (str.match(/[^\w]/) || str === "_")
+            ) {
+              // If single special character or _
+              return str;
+            } else {
+              return lorem
+                .split(" ")
+                .sort(() => (Math.random() > 0.5 ? 1 : -1))
+                .join(" ")
+                .substr(0, length);
+            }
+          })
+          .join("");
+      });
+    }
 
-		function replaceTextWithScribble() {
-			const style = document.createElement('style');
-			style.textContent = `
+    function replaceTextWithScribble() {
+      const style = document.createElement("style");
+      style.textContent = `
 				@font-face {
 					font-family: "Redacted Script";
-					src: url("${env.runtime.getURL('fonts/redacted-script-regular.woff')}");
+					src: url("${env.runtime.getURL("fonts/redacted-script-regular.woff")}");
 				}
 				*,*:after,*:before {
 					font-family: 'Redacted Script'!important;
 				}
 			`;
-			document.head.appendChild(style);
-			addOperation(() => style.remove());
-		}
+      document.head.appendChild(style);
+      addOperation(() => style.remove());
+    }
 
-		function undo() {
-			operations.forEach(op => op());
-			operations = [];
-		}
+    function undo() {
+      operations.forEach(op => op());
+      operations = [];
+    }
 
-		function runLoremiscous(options) {
-			if (operations.length) {
-				undo();
-			} else {
-				if (options.includes('replaceText')) {
-					requestIdleCallback(replaceText);
-				}
-				if (options.includes('replaceTextWithScribble')) {
-					requestIdleCallback(replaceTextWithScribble);
-				}
-				if (options.includes('replaceImage')) {
-					requestIdleCallback(replaceImgs);
-				}
-				if (options.includes('replaceBgImage')) {
-					requestIdleCallback(replaceBgImgs);
-				}
-				if (options.includes('replaceIframe')) {
-					requestIdleCallback(handleIframes);
-				}
-				// TODO: Replacing form control values can trigger unexpected behavior.
-				if (options.includes('replaceInput')) {
-					requestIdleCallback(handleFormInputs);
-				}
-			}
-		}
-		return runLoremiscous;
-	})();
+    function runLoremiscous(options) {
+      if (operations.length) {
+        undo();
+      } else {
+        if (options["replaceText"]) {
+          requestIdleCallback(() => {
+            replaceText({
+              textLengthMultiplier: options["textLengthMultiplier"]
+            });
+          });
+        }
+        if (options["replaceTextWithScribble"]) {
+          requestIdleCallback(replaceTextWithScribble);
+        }
+        if (options["replaceImage"]) {
+          requestIdleCallback(replaceImgs);
+        }
+        if (options["replaceBgImage"]) {
+          requestIdleCallback(replaceBgImgs);
+        }
+        if (options["replaceIframe"]) {
+          requestIdleCallback(handleIframes);
+        }
+        // TODO: Replacing form control values can trigger unexpected behavior.
+        if (options["replaceInput"]) {
+          requestIdleCallback(handleFormInputs);
+        }
+      }
+    }
+    return runLoremiscous;
+  })();
 
 // window.loremiscous();
